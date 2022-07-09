01c7f131214411fdc45d671a299a233aef311490
Dmitry Kalinin <kalinin.mitko@gmail.com>
2022-07-09 21:19:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email=AUTHOR_EMAIL,
     description=DESCRIPTION,
     long_description=read("README.rst"),
-    version="1.2.5",
+    version="1.2.6",
     license="MIT",
     url=URL,
     packages=find_packages(),
